8c7d975be1bc32c16c7835a5106de9f220ff596c
Artiom-Evs <evs.a.s@mail.ru>
2023-11-12 02:11:47 +0100

    Add IDataRepository<T> interface
    This interface provides access to the data access layer between the domain and the ORM-specific data provisioning layer.

---
+++ ICollector.Server/Services/IDataRepository.cs
@@ -0,0 +1,19 @@
+﻿using ICollector.Server.Models;
+using System.Linq.Expressions;
+
+namespace ICollector.Server.Services;
+
+public interface IDataRepository<T> 
+    where T : ModelBase
+{
+    IQueryable<T> Query();
+
+    Task SaveChangesAsync();
+
+    Task<bool> AnyAsync(Expression<Func<T, bool>> predicate);
+    Task<IEnumerable<T>> GetAsync();
+    Task<T?> GetAsync(Expression<Func<T, bool>> predicate);
+    Task<T> CreateAsync(T item);
+    Task UpdateAsync(T item);
+    Task DeleteAsync(Expression<Func<T, bool>> predicate);
+}
