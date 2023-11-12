05289cb477ef3cea591fa3ebb0b6b0bbddc9b3a6
Artiom-Evs <evs.a.s@mail.ru>
2023-11-12 21:47:17 +0100

    Edit CollectionItem class
    Created and Edited properties have been added.
    New database migration has been added.
    Initial data has also been edited.

--- ICollector.Server/Models/CollectionItem.cs
+++ ICollector.Server/Models/CollectionItem.cs
@@ -5,6 +5,8 @@ namespace ICollector.Server.Models;
 public class CollectionItem : ModelBase
 {
     public string Name { get; set; } = string.Empty;
+    public DateTime Created { get; set; }
+    public DateTime Edited { get; set; }
 
     [JsonIgnore]
     public UserCollection? Collection { get; set; }
