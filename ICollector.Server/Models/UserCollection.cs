fa09477a345f998349c24025e9c0d157f032afb6
Artiom-Evs <evs.a.s@mail.ru>
2023-11-12 22:37:09 +0100

    Edit UserCollection class
    Created and Edited properties have been added.
    New database migration has been added.
    Initial data has also been edited to match the new data model properties.

--- ICollector.Server/Models/UserCollection.cs
+++ ICollector.Server/Models/UserCollection.cs
@@ -4,6 +4,8 @@ public class UserCollection : ModelBase
 {
     public string Name { get; set; } = string.Empty;
     public string Description { get; set; } = string.Empty;
+    public DateTime Created { get; set; }
+    public DateTime Edited { get; set; }
     public string AuthorId { get; set; } = string.Empty;
 
     public List<CollectionItem> Items { get; set; } = new();
