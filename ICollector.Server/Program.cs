1ffcc32295f6a70025718b27bf134d71a239f49d
Artiom-Evs <evs.a.s@mail.ru>
2023-11-14 03:18:21 +0100

    Edit Program.cs
    Weather forecast code example  has been deleted.

--- ICollector.Server/Program.cs
+++ ICollector.Server/Program.cs
@@ -46,36 +46,10 @@ app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapGroup("/api/identity").MapIdentityApi<AppUser>();
-
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
-
-app.MapControllers();
 app.MapFallbackToFile("/index.html");
+app.MapControllers();
 
 app.SeedUsersData();
 app.SeedCollectionsData();
 
 app.Run();
-
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
