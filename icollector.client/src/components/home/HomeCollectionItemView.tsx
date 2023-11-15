ce85e333b0abbc19cf93f115145b4a6f34fb3432
Artiom-Evs <evs.a.s@mail.ru>
2023-11-15 02:37:08 +0100

    Add HomeCollectionItemView component
    This component is a presentation of one collection item on the home page.

---
+++ icollector.client/src/components/home/HomeCollectionItemView.tsx
@@ -0,0 +1,30 @@
+import { FormattedMessage } from "react-intl";
+import { CollectionItemType } from "../../services/ItemsDataService";
+
+interface HomeCollectionItemViewProps {
+    item: CollectionItemType
+}
+
+export function HomeCollectionItemView(props: HomeCollectionItemViewProps) {
+    return (
+        <div className="card mb-2">
+            <div className="card-body row">
+                <div className="col-sm-2" style={{ textAlign: "right" }}>
+                    <p>
+                        <FormattedMessage id="likes" values={{ c: 0 }} />
+                        <br />
+                        <FormattedMessage id="comments" values={{ c: 0 }} />
+                    </p>
+                </div>
+                <div className="col-sm-7">
+                    {props.item.name}
+                </div>
+                <div className="col-sm-3">
+                    <FormattedMessage id="created" values={{ d: new Date(props.item.created) }} />
+                    <br />
+                    <FormattedMessage id="edited" values={{ d: new Date(props.item.edited) }} />
+                </div>
+            </div>
+        </div>
+    )
+}
