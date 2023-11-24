87d470fcb0585ed312083268e9a90fc7cd6f5e98
Artiom-Evs <evs.a.s@mail.ru>
2023-11-24 17:20:33 +0100

    Add SearchController class
    This class provides application search API.

---
+++ ICollector.Server/Controllers/SearchController.cs
@@ -0,0 +1,37 @@
+﻿using ICollector.Server.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ICollector.Server.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class SearchController : Controller
+{
+    private readonly ISearchService _search;
+
+    public SearchController(ISearchService search)
+    {
+        _search = search;
+    }
+
+    [HttpGet("Items")]
+    public async Task<IActionResult> SearchItemsAsync(string text)
+    {
+        var items = await _search.SearchItemsAsync(text);
+        return Ok(items);
+    }
+
+    [HttpGet("ItemComments")]
+    public async Task<IActionResult> SearchItemCommentsAsync(string text)
+    {
+        var items = await _search.SearchItemCommentsAsync(text);
+        return Ok(items);
+    }
+
+    [HttpGet("Collections")]
+    public async Task<IActionResult> SearchCollectionsAsync(string text)
+    {
+        var items = await _search.SearchCollectionsAsync(text);
+        return Ok(items);
+    }
+}
