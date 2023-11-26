110c2ae3e8c62e90eb35cd91dad249460a9dab14
Artiom-Evs <evs.a.s@mail.ru>
2023-11-26 17:50:48 +0100

    Edit SearchController class
    Search API endpoints now returns API models instead domain models.

--- ICollector.Server/Models/ApiModels/ItemCommentResponse.cs
+++ ICollector.Server/Models/ApiModels/ItemCommentResponse.cs
@@ -1,8 +1,11 @@
-﻿namespace ICollector.Server.Models;
+﻿using ICollector.Server.Models.ApiModels;
+
+namespace ICollector.Server.Models;
 
 public class ItemCommentResponse
 {
     public int Id { get; set; }
+    public ItemResponse? Item { get; set; }
     public int ItemId { get; set; }
     public string AuthorId { get; set; } = string.Empty;
     public string AuthorName { get; set; } = string.Empty;
