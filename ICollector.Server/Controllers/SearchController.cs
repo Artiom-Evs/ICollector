4aa5c3324d278cff4961491bd5fe8ced839049b8
Artiom-Evs <evs.a.s@mail.ru>
2023-11-26 18:51:07 +0100

    Edit SearchController class
    Search API endpoints now return API models with information about related user names.

--- ICollector.Server/Controllers/SearchController.cs
+++ ICollector.Server/Controllers/SearchController.cs
@@ -1,25 +1,45 @@
 ﻿using ICollector.Server.Extensions;
+using ICollector.Server.Models;
 using ICollector.Server.Services;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-
+using Microsoft.EntityFrameworkCore;
 namespace ICollector.Server.Controllers;
+
 
 [ApiController]
 [Route("api/[controller]")]
 public class SearchController : Controller
 {
     private readonly ISearchService _search;
+    private readonly UserManager<AppUser> _userManager;
 
-    public SearchController(ISearchService search)
+    public SearchController(ISearchService search, UserManager<AppUser> userManager)
     {
         _search = search;
+        _userManager = userManager;
     }
 
     [HttpGet("Items")]
     public async Task<IActionResult> SearchItemsAsync(string text)
     {
         var items = await _search.SearchItemsAsync(text);
-        var itemResponses = items.Select(i => i.ToApiResponse()).ToArray();
+        var authorIds = items.Select(c => c.Collection?.AuthorId ?? "");
+        var authors = await _userManager.Users
+            .Where(u => authorIds.Contains(u.Id))
+            .Select(u => new { Id = u.Id, Name = u.UserName })
+            .ToArrayAsync();
+
+        var itemResponses = items
+            .Select(i =>
+            {
+                var response = i.ToApiResponse();
+                if (response.Collection != null)
+                    response.Collection.AuthorName = authors.FirstOrDefault(u => u.Id == response.Collection.AuthorId)?.Name ?? "unknown";
+                return response;
+            })
+            .ToArray();
+
         return Ok(itemResponses);
     }
 
@@ -27,7 +47,21 @@ public class SearchController : Controller
     public async Task<IActionResult> SearchItemCommentsAsync(string text)
     {
         var comments = await _search.SearchItemCommentsAsync(text);
-        var commentResponses = comments.Select(i => i.ToApiResponse()).ToArray();
+        var authorIds = comments.Select(c => c.AuthorId);
+        var authors = await _userManager.Users
+            .Where(u => authorIds.Contains(u.Id))
+            .Select(u => new { Id = u.Id, Name = u.UserName })
+            .ToArrayAsync();
+
+        var commentResponses = comments
+            .Select(c =>
+            {
+                var response = c.ToApiResponse();
+                response.AuthorName = authors.FirstOrDefault(u => u.Id == response.AuthorId)?.Name ?? "unknown";
+                return response;
+            })
+            .ToArray();
+
         return Ok(commentResponses);
     }
 
@@ -35,7 +69,20 @@ public class SearchController : Controller
     public async Task<IActionResult> SearchCollectionsAsync(string text)
     {
         var collections = await _search.SearchCollectionsAsync(text);
-        var collectionResponses = collections.Select(i => i.ToApiResponse()).ToArray();
+        var authorIds = collections.Select(c => c.AuthorId);
+        var authors = await _userManager.Users
+            .Where(u => authorIds.Contains(u.Id))
+            .Select(u => new { Id = u.Id, Name = u.UserName })
+            .ToArrayAsync();
+
+        var collectionResponses = collections
+            .Select(c =>
+            {
+                var response = c.ToApiResponse();
+                response.AuthorName = authors.FirstOrDefault(u => u.Id == response.AuthorId)?.Name ?? "unknown";
+                return response;
+            })
+            .ToArray();
 
         return Ok(collectionResponses);
     }
